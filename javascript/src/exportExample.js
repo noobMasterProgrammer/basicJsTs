71ae3254577fdd93bdab20f5c0e79b0ab1aa4e04
Abhishek1009 <ghoshabhishek1640@gmail.com>
2020-10-18 22:57:34 +0530

    commit

---
+++ javascript/src/exportExample.js
@@ -0,0 +1,11 @@
+const PI = 3.14;
+const area = function (r) {
+    return PI * r * r;
+}
+
+module.exports = {
+    PI,area
+}
+
+// module.exports.PI = PI;
+// module.exports.area = area;
---
+++ javascript/src/importExample.js
@@ -0,0 +1,6 @@
+let exportExample = require('./exportExample');
+
+console.log(exportExample.PI);
+console.log(exportExample.area(10));
+
+
