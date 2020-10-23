e739074a58060337e05cd722a072563119705501
Abhishek1009 <ghoshabhishek1640@gmail.com>
2020-10-23 23:13:49 +0530

    commit

--- AngularTutorial/src/app/sample/sample.component.ts
+++ AngularTutorial/src/app/sample/sample.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SampleComponent implements OnInit {
 
+  name = 'Abhishek Ghosh';
+  courses = ['course-1', 'course-2', 'course-3', 'course-4', 'course-5'];
+
   constructor() { }
+  getName(): string {
+    return this.name;
+  }
 
   ngOnInit(): void {
   }
