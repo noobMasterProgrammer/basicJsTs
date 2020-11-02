e366e5fe36ab89e38dc1b5084aca953b2c056629
Abhishek1009 <ghoshabhishek1640@gmail.com>
2020-11-03 01:09:38 +0530

    binding added

--- AngularTutorial/src/app/app-routing.module.ts
+++ AngularTutorial/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SampleComponent } from './sample/sample.component';
+import { BindingComponent } from './binding/binding.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/sample', pathMatch: 'full'},
-  {path: 'sample', component: SampleComponent , pathMatch: 'full'}];
+  {path: 'sample', component: SampleComponent, pathMatch: 'full' },
+  {path: 'binding', component: BindingComponent , pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
