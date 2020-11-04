184a8baf167635a18699e184ccd3cae2dd695f91
Abhishek1009 <ghoshabhishek1640@gmail.com>
2020-11-04 10:20:00 +0530

    commit

--- AngularTutorial/src/app/binding/binding.component.ts
+++ AngularTutorial/src/app/binding/binding.component.ts
@@ -9,6 +9,7 @@ export class BindingComponent implements OnInit {
 
   public title = 'This is a binding example';
   public colspan = 5;
+  public isActive = true;
   public imageUrl = 'https://avatars3.githubusercontent.com/u/31528255?s=460&u=26403ba4769620c756f93105ac6d575c22c9ff14&v=4';
   constructor() { }
 
